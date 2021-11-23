da293fc3720f500ea0bdd12c1281c83abace5563
DiCa! <diecau20@gmail.com>
2021-11-23 09:05:55 -0300

    Renderizado de Img, Title y Url

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,17 +1,39 @@
 import React, { useState } from 'react'
+import PropTypes from 'prop-types';
 
-const AddCategory = () => {
+const AddCategory = ({ setCategories }) => {
 
-    const [inputValue, setInputValue] = useState('Hola Mundo')
+    const [inputValue, setInputValue] = useState('');
 
+    const handleInputChange = ( e ) => {
+        setInputValue( e.target.value )
+    }
+
+    const handleSubmit = ( e ) => {
+        e.preventDefault();
+
+        if( inputValue.trim().length > 2 ) {
+            setCategories( cats => [ ...cats, inputValue ])
+            setInputValue(''); // para limpiar el input y luego ENTER
+        }
     
+    }
+
     return (
-        <>
+        <form onSubmit= { handleSubmit }>
            <input 
             type="text"
+            value= { inputValue }
+            onChange= { handleInputChange }
            />
-        </>
+        </form>
     )
 }
+
+
+AddCategory.propTypes = {
+    setCategories: PropTypes.func.isRequired
+}
+
 
 export default AddCategory
---
+++ src/components/GifGridItem.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const GifGridItem = ({ id, title, url }) => {
+
+    //console.log( id, title, url )
+
+    return (
+        <>
+            <img src={ url } alt={ title }/>
+            <p>{ title }</p>
+        </>
+    )
+}
+
+export default GifGridItem
