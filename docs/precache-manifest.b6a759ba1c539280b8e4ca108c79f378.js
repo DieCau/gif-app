7400138dde3010df4ddfaed302a3f188fd1a3c03
Diego Caucota <diegocaucota@gmail.com>
2021-11-23 23:36:18 -0300

    docs agregado

---
+++ docs/precache-manifest.b6a759ba1c539280b8e4ca108c79f378.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f9018fae7755fb85044d92525834ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b57288015111ca6a297",
+    "url": "/static/css/main.1410bcce.chunk.css"
+  },
+  {
+    "revision": "ec7c562017080769741b",
+    "url": "/static/js/2.62e13cf1.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.62e13cf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b57288015111ca6a297",
+    "url": "/static/js/main.47bcaf4a.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
