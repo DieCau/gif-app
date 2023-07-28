d728fe4aabbfbe9d33f8568e2659931debac7ecb
Diego Caucota <diegocaucota@gmail.com>
2023-07-28 18:16:35 -0300

    agrego archivo de barril

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,17 +1,14 @@
 import { useState } from "react";
-import { AddCategory } from "./components/AddCategory";
-import { GifGrid } from "./components/GifGrid";
+import { AddCategory, GifGrid } from './components';
 
 export const GifApp = () => {
 
     const [ categories, setCategories ] = useState([ 'One Punch' ]);
     
     const onAddCategory = ( newCategory ) => {
-        
+        if( categories.includes(newCategory)) return
         setCategories([ newCategory, ...categories ])
-    
     }
-
 
 
     return (
